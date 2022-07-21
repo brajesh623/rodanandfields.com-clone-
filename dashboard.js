c190c312e8aa9a4c88a80805476ae47df587038a
Krushana-chavan <krushnachavan29@gmail.com>
2022-07-22 02:33:27 +0530

    updated

---
+++ dashboard.js
@@ -0,0 +1,243 @@
+let mainnavbar=()=>{
+    return `  <div id="navbar">
+    <p>FIND A CONSULT</p>
+    <H2>RODAN+FIELDS</H2>
+    <div id="navbarend">
+        <div>
+            <span>Search</span>
+            <span id="navsp"><img
+                    src="https://th.bing.com/th/id/R.11aac9c1f6fe373171cb05d99f8a9a08?rik=0s2xll77abHQSQ&riu=http%3a%2f%2fcdn.onlinewebfonts.com%2fsvg%2fimg_176563.png&ehk=LBggy%2frXkomPPhWq39S4339%2bZgz%2fRJ10%2bpzyO8XEFYE%3d&risl=&pid=ImgRaw&r=0"
+                    alt=""></span>
+        </div>
+        <div>
+            <a href="signin.html">Signin</a>
+            <span id="navsp"><img
+                    src="https://th.bing.com/th/id/OIP.BVQV8VO63bEsw8x-1FGWnAHaHa?pid=ImgDet&w=1000&h=1000&rs=1"
+                    alt=""></span>
+        </div>
+        <div>
+            <span id="navsp"><img
+                    src="https://cdn2.iconfinder.com/data/icons/shopping-316/100/Shopping_7-512.png"
+                    alt=""></span>
+        </div>
+        <div>
+            <span id="navsp"><img
+                    src="https://th.bing.com/th/id/R.68861cc3f32b5c522eb33baae1b7e67f?rik=WRn9vJTy%2fbfx3Q&riu=http%3a%2f%2ficons.iconarchive.com%2ficons%2fwikipedia%2fflags%2f1024%2fIN-India-Flag-icon.png&ehk=yynj9tUxh29yIVJtvCVAL%2bae4tuPlvLlYklBp4nmd98%3d&risl=&pid=ImgRaw&r=0"
+                    alt=""></span>
+            <a href="">INDIA</a>
+        </div>
+
+    </div>
+</div>
+<hr>
+<div id="navbarsecond">
+    <a href="">SHOP</a>
+    <a href="">BEST SELLERS</a>
+    <a href="">FEATURED</a>
+    <a href="">OUR STORY</a>
+    <a href="">PERKS PROGRAM</a>
+    <a href="">BECOME A CONSULTANT BLOG</a>
+
+</div>`
+}
+
+let lastbar =()=>{
+    return `      <a href="">@2022 Rodan & Fields</a>
+    <a href=""> Privacy Policy</a> 
+    I
+    <a href="">Terms & Conditions</a>
+    I
+    <a href="">Accessibillity</a>
+    I
+    <a href="">California Supply Chains Act</a>
+    I
+    <a href="">DSA Code Of Ethics</a>
+    I
+    <a href="">Disclaimer</a>
+    I
+    <a href="">Preference Center</a>
+     `
+
+}
+let footer =()=>{
+    return` <div id="onef">
+    <p>ABOUT R+F</p>
+    <div><a href="">Press Room</a></div>
+    <div> <a href="">Company info</a></div>
+    <div><a href="">Careers</a></div>
+    <div> <a href="">R+F Recycling Program</a></div>
+    <div><a href="">Blog</a></div>
+
+
+</div>
+<div id="secf">
+    <p>HELP</p>
+    <div><a href="">Product & Help Center</a></div>
+    <div><a href="">Shipping</a></div>
+    <div><a href="">Satisfaction</a></div>
+    <div><a href="">R+F FAQS</a></div>
+    <div><a href="">Klarna</a></div>
+    <div><a href="">Site Map</a></div>
+
+</div>
+<div id="thrf">
+    <h2>Let's get Social</h2>
+    <div id="fimg">
+        <img src="https://th.bing.com/th/id/R.4c25d18059353799aff91dcdd8f19bb4?rik=IfUe0Gz8GFe5Bg&riu=http%3a%2f%2fwww.clipartbest.com%2fcliparts%2fjcx%2f6e6%2fjcx6e6Egi.png&ehk=rOSPC0O0jsW7liSBIxU2ErzKYXzV1QhkiBfkhEB6oNI%3d&risl=&pid=ImgRaw&r=0"
+            alt="">
+        <img src="https://th.bing.com/th/id/R.8eb68c7065e5057c9314f335d1bcb4a6?rik=jN4%2f3c2j9%2fNquA&riu=http%3a%2f%2fpluspng.com%2fimg-png%2ftwitter-png-twitter-icon-1600.png&ehk=cfVuY5vaxmKssnSiLi2qsApgVuOQ2a3R0fEel7T0mJk%3d&risl=&pid=ImgRaw&r=0"
+            alt="">
+        <img src="https://www.freeiconspng.com/uploads/black-instagram-icon-vector-22.png" alt="">
+        <img src="https://th.bing.com/th/id/R.fee638d128e02d055c72ef97561b2565?rik=Ew2JRCA2g0f1sg&riu=http%3a%2f%2fpixsector.com%2fcache%2f5ad86f3d%2favc6f87c75c95a708cd60.png&ehk=OXz54wny8Uw2%2beLxwg8c%2fKrywMwzoGf5oG8v3VCoLfM%3d&risl=&pid=ImgRaw&r=0"
+            alt="">
+        <img src="https://www.freeiconspng.com/uploads/pinterest-icon-black-2.png" alt="">
+    </div>
+</div>`
+}
+export {mainnavbar,footer,lastbar}
+
+let arraydata =[
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/ENFM030-Thumbnail-458x458.jpg?context=bWFzdGVyfGltYWdlc3wyNTQ2MHxpbWFnZS9qcGVnfGltYWdlcy9oZmQvaGQ2LzE0ODgyMTk1ODk4Mzk4LmpwZ3wyYThhMjQ0NTc4YjI1ZmQ1ODY3MDE5NGMyYjk3MjA2MTRmNDQ2MzdmOGVhYzMxYTFmMDk1YmIyY2Q5NDhjMzIw",
+        type:"Best Seller",
+        title:"Total RF Serum",
+        desc:"Energizes millions of surface skin cells by 200% to address all key signs of skin aging",
+        retail_price:175,
+        value_price:"Retail Price",
+        rating:4.5,
+        people_rated:7311,
+
+
+    },
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/ENHLSH01-458x458.jpg?context=bWFzdGVyfGltYWdlc3w0MDkxfGltYWdlL2pwZWd8aW1hZ2VzL2gzNS9oODgvMTM5Nzc1NDQ2ODc2NDYuanBnfDlmYTEwMTFkMjFiNzMwYjlhNjU0NTNiYjU5ZDBmMGY3ZWIzNmNiZmYwMTJhMGYzMTFkMzA1ODhkOGI1MjdlMmE",
+        type:"Best Seller",
+        title:"R + F Lash Boost",
+        desc:"Nightly conditioning serum gives the appearance of longer, darker-looking lashes",
+        retail_price:155,
+        value_price:"Retail Price",
+        rating:4.6,
+        people_rated:(7996),
+
+
+    },
+    
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/ENHLSH01-458x458.jpg?context=bWFzdGVyfGltYWdlc3w0MDkxfGltYWdlL2pwZWd8aW1hZ2VzL2gzNS9oODgvMTM5Nzc1NDQ2ODc2NDYuanBnfDlmYTEwMTFkMjFiNzMwYjlhNjU0NTNiYjU5ZDBmMGY3ZWIzNmNiZmYwMTJhMGYzMTFkMzA1ODhkOGI1MjdlMmE",
+        type:"Best Seller",
+        title:"ESSENTIALS Face + Body Sunscreen Broad Spectrum SPF 50",
+        desc:"Provides broad spectrum protection to help defend agains UV rays",
+        retail_price:34,
+        value_price:"Retail Price",
+        rating:4.9,
+        people_rated:(7311),
+
+
+    },
+    
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/ESFB150-208x208-mobile-1.jpg?context=bWFzdGVyfGltYWdlc3w0NDM3fGltYWdlL2pwZWd8aW1hZ2VzL2g0MC9oY2MvMTAyOTE4Mjk0NDA1NDIuanBnfDFhM2EwMGZhYWIwMTBkZWE4OTljMDcyMzYyNDNkMDUzNWMzZGU1OGE0ZDVlNTRjZGMyMWY0MmFlZjE3NzE0OGQ",
+        type:" Best Seller",
+        title:"REDEFINE Multi-Function Eye Cream",
+        desc:"Visibly firms + improves all eye-area wrinkles to restore youthful,natural contours",
+        retail_price:70,
+        value_price:"Retail Price",
+        rating:4.6,
+        people_rated:(9461),
+
+
+    },
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/HAAGR-458x458.jpg?context=bWFzdGVyfGltYWdlc3w5MjA3fGltYWdlL2pwZWd8aW1hZ2VzL2gzZC9oMzMvMTM5Nzc1NDM5MDEyMTQuanBnfDU5YTY2OWVkYTZjODM5NzY4YTVhZmJlMGNjZDQzNDU1MzUzMWY2MmUzMzY4ZTkxMTgyMWIwYThkNjYxZGRjMzc",
+        type:" Best Seller",
+        title:"REDEFINE Regimen",
+        desc:"Award-winning anti-aging routine visibly lifts + firms for younger-looking skin",
+        retail_price:220,
+        value_price:"Retail Price",
+        rating:4.7,
+        people_rated:(7311),
+
+
+    },
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/HUNRJ001-458x458.jpg?context=bWFzdGVyfGltYWdlc3w5ODg3fGltYWdlL2pwZWd8aW1hZ2VzL2g1ZC9oNGUvMTM5Nzc1NDM2MzkwNzAuanBnfDU5ZTJkYmVkZTg2MTRlOGU1NTg3NjkxYjVjZGYwZjBjZjY4ZTk4YTBiZTYzN2UxZmJjZTE5ZmJhM2I0ZGVhNGM",
+        type:" Best Seller",
+        title:"UNBLEMISH Regimen",
+        desc:"Fight adult acne, uneven skin tone + signs of aging with this potent 4-step routine",
+        retail_price:190,
+        value_price:"Retail Price",
+        rating:4.6,
+        people_rated:(461),
+
+
+    },
+
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/ENHEFG1-570x570-Desktop.png?context=bWFzdGVyfGltYWdlc3wyMjI3N3xpbWFnZS9wbmd8aW1hZ2VzL2g2NS9oYjUvMTQ1MzIxNjY4MTE2NzgucG5nfGNhNTQzZTUyNmU3NTMxZjAxZTc5NThjMmQ3MzU0YzVkNTY1OTExMThjNWEyMDBiYTIwMTFhNWM1Yzk4YTk3ODI",
+        type:" Best Seller",
+        title:"R + F Active Hydratio Serum",
+        desc:"Instantly boosts hydration levels by 200% + locks in moisture for bouncy",
+        retail_price:112,
+        value_price:"Retail Price",
+        rating:4.8,
+        people_rated:(8798),
+
+
+    },
+
+    {
+       
+        image:"https://www.rodanandfields.com/en-us/medias/ENBR001-458x458.jpg?context=bWFzdGVyfGltYWdlc3w3Nzc0fGltYWdlL2pwZWd8aW1hZ2VzL2g3MS9oMmQvMTM5Nzc1NDMzNzY5MjYuanBnfDMxMTEyY2E3ZDMzNTVjZDA0OGRmODE4NTk4NmNkZjcyMTQwNGM3NDMxNTRkNGEyNTI5MDI2ZTM4OWRkZmEzMzc",
+        type:" Best Seller",
+        title:"Rodan + fields Active Hydration Body Replenish",
+        desc:"Breakthrough body moisturizer instantly + continuosly hydrates skin",
+        retail_price:66,
+        value_price:"Retail Price",
+        rating:4.9,
+        people_rated:(7129),
+
+
+    },
+    
+]
+
+console.log(arraydata)
+dispalyproduct(arraydata)
+
+function dispalyproduct(arraydata){
+    arraydata.forEach(el =>{
+
+  let div=document.createElement("div")
+  let label=document.createElement("p")
+  label.innerText=el.type
+  let image=document.createElement("img")
+  image.src=el.image
+  let caption=document.createElement("h4")
+  caption.innerText=el.title
+  let description=document.createElement("p")
+  description.innerText=el.desc
+  let price=document.createElement("p")
+  price.innerText= "$"+el.retail_price
+  let retail_value=document.createElement("p")
+  retail_value.innerText= el.value_price
+  let addtobag=document.createElement("button")
+
+  addtobag.innerText="ADD TO BAG"
+  addtobag.addEventListener("click", function (e) {
+      
+    let addtobagstorage = JSON.parse(localStorage.getItem("Userbag")) || []      
+        addtobagstorage.push(el)
+        localStorage.setItem("Userbag", JSON.stringify(addtobagstorage))
+      })
+  div.append( label,image, caption, description, price,retail_value,addtobag)
+  document.getElementById("shopbestsellr").append(div)
+
+  })
+}
